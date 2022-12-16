5976bf80449f489c055167b6ce3b2efc3c7c8e2f
Dennis <mhulla1981@googlemail.com>
2022-12-16 08:56:27 +0100

    clippy&fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::{Parser, Subcommand};
 use genetics::*;
-use rand_distr::Normal;
-use serde::{Serialize, Deserialize};
-use std::time::Duration;
 use ndarray::*;
+use rand_distr::Normal;
+use serde::{Deserialize, Serialize};
+use std::time::Duration;
 use uuid::Uuid;
 
 //----------------------------------------------
@@ -14,39 +14,40 @@ struct MyPayload {
     experiment: Uuid,
 }
 
-fn rand(min: u32, max: u32) -> u32 {
+/*fn rand(min: u32, max: u32) -> u32 {
     min + rand::random::<u32>() % (max - min)
-}
+}*/
 
 async fn spawn_new_genes(genepool: &mut GenePool<MyPayload>) -> Result<(), GenomeError> {
-  
-  let experiment = Uuid::new_v4();
-  println!("Experiment UUID is {}", experiment);
+    let experiment = Uuid::new_v4();
+    println!("Experiment UUID is {}", experiment);
 
-  for _ in 0..genepool.population_size {
+    for _ in 0..genepool.population_size {
         genepool
-            .add_genome(Genome::new(0, MyPayload {
-                botnet: BotNet::new(7, 50, 4),
-                experiment,
-            }))
+            .add_genome(Genome::new(
+                0,
+                MyPayload {
+                    botnet: BotNet::new(7, 50, 4),
+                    experiment,
+                },
+            ))
             .await?;
     }
-    
+
     Ok(())
 }
 
 fn breed_next_generation(
-    genes: &Vec<Genome<MyPayload>>,
+    genes: &[Genome<MyPayload>],
 ) -> Result<Vec<Genome<MyPayload>>, GenomeError> {
     println!("Breeding next generation!");
 
     let mut new_genes = Vec::<Genome<MyPayload>>::new();
 
     let mut total_fitness: f32 = 0.0;
-    
+
     // Only preserve the 10 fittest individuals
-    for x
-     in &genes[0..40] {
+    for x in &genes[0..40] {
         total_fitness += x.message.fitness.unwrap();
         println!(
             "Genome {} has fitness {}",
@@ -56,21 +57,23 @@ fn breed_next_generation(
 
         // Keep this network as it is
         new_genes.push(Genome::new(
-          x.message.generation + 1,
-          MyPayload {
-            botnet: x.message.payload.botnet.clone(),
-            experiment: x.message.payload.experiment,
-          }));
+            x.message.generation + 1,
+            MyPayload {
+                botnet: x.message.payload.botnet.clone(),
+                experiment: x.message.payload.experiment,
+            },
+        ));
 
         // Create 10 variants of it
         for variant in 0..14 {
             let dist = Normal::new(0.0, 0.05 + (variant as f32) * 0.005).unwrap();
             new_genes.push(Genome::new(
-              x.message.generation + 1,
-              MyPayload {
-                botnet: x.message.payload.botnet.variant(&dist),
-                experiment: x.message.payload.experiment,
-              }));
+                x.message.generation + 1,
+                MyPayload {
+                    botnet: x.message.payload.botnet.variant(&dist),
+                    experiment: x.message.payload.experiment,
+                },
+            ));
         }
     }
     println!("Total fitness was {}", total_fitness);
@@ -81,10 +84,10 @@ fn breed_next_generation(
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-  #[arg(short, long, default_value = "amqp://guest:guest@127.0.0.1:5672")]
+    #[arg(short, long, default_value = "amqp://guest:guest@127.0.0.1:5672")]
     pool: String,
 
-   #[command(subcommand)]
+    #[command(subcommand)]
     command: Commands,
 }
 
@@ -107,37 +110,48 @@ async fn main() {
     let v: f32 = 250.0 + rand::random::<f32>() * 1750.0;
     tokio::time::sleep(Duration::from_millis(v as u64)).await;
 
-
     let population_size = 600;
 
     match cli.command {
-      Commands::Dump => {
-        let mut genepool = GenePool::<MyPayload>::new(population_size, FitnessSortingOrder::LessIsBetter, cli.pool).unwrap();
-        genepool.dump().unwrap();
-      },
-      Commands::Reset => {
-        //println!("RESET DISABLED FOR NOW!");
-         
-        let mut genepool = GenePool::<MyPayload>::new(population_size, FitnessSortingOrder::LessIsBetter, cli.pool).unwrap();
-        genepool.empty_pool().unwrap();
-        spawn_new_genes(&mut genepool).await.unwrap(); 
-      },
-      Commands::Run => {
-        let mut handles: Vec<tokio::task::JoinHandle<Result<(), GenomeError>>> = vec![];
+        Commands::Dump => {
+            let mut genepool = GenePool::<MyPayload>::new(
+                population_size,
+                FitnessSortingOrder::LessIsBetter,
+                cli.pool,
+            )
+            .unwrap();
+            genepool.dump().unwrap();
+        }
+        Commands::Reset => {
+            //println!("RESET DISABLED FOR NOW!");
 
-        handles.push(tokio::spawn(async move {
-            //let population_handler = MyPopulationHandler::new();
-            let mut genepool = GenePool::<MyPayload>::new(population_size, FitnessSortingOrder::LessIsBetter, cli.pool).unwrap();
-    
-            genepool.monitor(breed_next_generation).await?;
-    
-            Ok(())
-        }));
+            let mut genepool = GenePool::<MyPayload>::new(
+                population_size,
+                FitnessSortingOrder::LessIsBetter,
+                cli.pool,
+            )
+            .unwrap();
+            genepool.empty_pool().unwrap();
+            spawn_new_genes(&mut genepool).await.unwrap();
+        }
+        Commands::Run => {
+            let mut handles: Vec<tokio::task::JoinHandle<Result<(), GenomeError>>> = vec![];
 
-        futures::future::join_all(handles).await;
-      }
+            handles.push(tokio::spawn(async move {
+                //let population_handler = MyPopulationHandler::new();
+                let mut genepool = GenePool::<MyPayload>::new(
+                    population_size,
+                    FitnessSortingOrder::LessIsBetter,
+                    cli.pool,
+                )
+                .unwrap();
+
+                genepool.monitor(breed_next_generation).await?;
+
+                Ok(())
+            }));
+
+            futures::future::join_all(handles).await;
+        }
     }
-    
-
-    
 }
